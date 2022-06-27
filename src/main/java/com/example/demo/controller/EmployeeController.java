5095fc04bc98db36f48062ae2149fc45dfc34dc3
rp1122334564 <ricardo.parada@publiqare.com>
2022-06-27 15:34:41 +0100

    created methods and templates for /new, /save

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.example.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -15,12 +17,26 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @GetMapping("/show")
+    @GetMapping({"/show", "/", "/list"})
     private ModelAndView show() {
         ModelAndView mav = new ModelAndView("list-employees");
         List<Employee> list = employeeRepository.findAll();
         mav.addObject("employees", list);
         return mav;
+    }
+
+    @GetMapping("/new")
+    private ModelAndView add() {
+        ModelAndView mav = new ModelAndView("add-employee-form");
+        Employee employee = new Employee();
+        mav.addObject("employee", employee);
+        return mav;
+    }
+
+    @PostMapping("/save")
+    private String save(@ModelAttribute Employee employee) {
+        employeeRepository.save(employee);
+        return "redirect:/list";
     }
 
 }
